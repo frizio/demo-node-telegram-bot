0ef69e1e628915996c5f2b56cf43ecbfa3a9cf69
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-03-14 16:11:11 +0100

    Launchthe  Bot and start command reply with a msg

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('Demo Node Telegram Bot with Telegraf');
+const dotnev = require("dotenv");
+const { Telegraf } = require("telegraf");
+const os = require("os");
+
+dotnev.config();
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start((ctx) => {
+  const hostname = os.hostname();
+  const msg = `Welcome from ${hostname}. Demo Node Telegram Bot with Telegraf.`;
+  ctx.reply(msg);
+});
+
+bot.launch();
