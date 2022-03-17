069ee840fe376c64dfbc9631b375b477d0579a79
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-03-17 20:17:42 +0100

    displays the inline keyboard buttons

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ dotnev.config();
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start((ctx) => {
+  console.log(ctx.from);
   const hostname = os.hostname();
   const msg = `Welcome from ${hostname}. Demo Node Telegram Bot with Telegraf.`;
   ctx.reply(msg);
@@ -45,13 +46,52 @@ bot.hashtag("sea", (ctx) => {
   ctx.reply("Hashtag sea!!!");
 });
 
+bot.hears(["animals", "Animals"], (ctx) => {
+  let animalMessage = `Great, here are pictures of animals you would love`;
+  ctx.deleteMessage();
+  bot.telegram.sendMessage(ctx.chat.id, animalMessage, {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: "Dog",
+            callback_data: "dog",
+          },
+          {
+            text: "Cat",
+            callback_data: "cat",
+          },
+        ],
+      ],
+    },
+  });
+});
+
+bot.action("dog", (ctx) => {
+  ctx.reply("DOG!");
+  /*
+  bot.telegram.sendPhoto(ctx.chat.id, {
+      source: "res/dog.jpeg"
+  })
+  */
+});
+
+bot.action("cat", (ctx) => {
+  ctx.reply("CAT!");
+  /*
+  bot.telegram.sendPhoto(ctx.chat.id, {
+      source: "res/cat.jpeg"
+  })
+  */
+});
+
 bot.launch({
   webhook: {
     domain: process.env.SERVER_URL,
-    port: process.env.PORT
-  }
+    port: process.env.PORT,
+  },
 });
 
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
